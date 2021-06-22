877d2233850c23cb2da10abc6df2150f8653923b
Razvan <razvanbugoi@gmail.com>
2021-06-22 17:16:17 +0100

    set up the initial project files and JS logic for grid generation

---
+++ script.js
@@ -0,0 +1,21 @@
+document.body.onload = addElements;
+const mainDiv = document.querySelector('div');
+
+function addElements(n) {
+  for (let i=0; i<n*n; i++) {
+    let divs = document.createElement('div')
+    divs.className = 'div-grids'
+    divs.style.border = "1px solid red"
+    mainDiv.appendChild(divs)
+  }
+    mainDiv.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+    mainDiv.style.gridTemplateRows = `repeat(${n}, 1fr)`;
+}
+
+function resetGrid() {
+    if (mainDiv.innerHTML == '') return
+    mainDiv.innerHTML = '';
+}
+
+
+
