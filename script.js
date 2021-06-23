289b577ea43d92936f0f959a734414a1a3e9efcb
Razvan <razvanbugoi@gmail.com>
2021-06-23 09:11:14 +0100

    made all the preset grid buttons functional

--- script.js
+++ script.js
@@ -1,3 +1,106 @@
+const ten = document.getElementById('ten');
+const twenty = document.getElementById('twenty');
+const thirty = document.getElementById('thirty');
+const forty = document.getElementById('forty');
+const fifty = document.getElementById('fifty');
+const sixty = document.getElementById('sixty');
+const seventy = document.getElementById('seventy');
+const eighty = document.getElementById('eighty');
+const ninety = document.getElementById('ninety');
+
+
+ten.onclick = addTenGrids;
+twenty.onclick = addTwentyGrids;
+thirty.onclick = addThirtyGrids;
+forty.onclick = addFortyGrids;
+fifty.onclick = addFiftyGrids;
+sixty.onclick = addSixtyGrids;
+seventy.onclick = addSeventyGrids;
+eighty.onclick = addEightyGrids;
+ninety.onclick = addNinetyGrids;
+
+function addTenGrids() {
+  if (mainDiv.innerHTML == '') {
+    addElements(10)
+  } else {
+    mainDiv.innerHTML = '';
+    addElements(10);
+  }
+}
+
+function addTwentyGrids() {
+  if (mainDiv.innerHTML == '') {
+    addElements(20)
+  } else {
+    mainDiv.innerHTML = '';
+    addElements(20);
+  }
+}
+
+function addThirtyGrids() {
+  if (mainDiv.innerHTML == '') {
+    addElements(30)
+  } else {
+    mainDiv.innerHTML = '';
+    addElements(30);
+  }
+}
+
+function addFortyGrids() {
+  if (mainDiv.innerHTML == '') {
+    addElements(40)
+  } else {
+    mainDiv.innerHTML = '';
+    addElements(40);
+  }
+}
+
+function addFiftyGrids() {
+  if (mainDiv.innerHTML == '') {
+    addElements(50)
+  } else {
+    mainDiv.innerHTML = '';
+    addElements(50);
+  }
+}
+
+function addSixtyGrids() {
+  if (mainDiv.innerHTML == '') {
+    addElements(60)
+  } else {
+    mainDiv.innerHTML = '';
+    addElements(60);
+  }
+}
+
+function addSeventyGrids() {
+  if (mainDiv.innerHTML == '') {
+    addElements(70)
+  } else {
+    mainDiv.innerHTML = '';
+    addElements(70);
+  }
+}
+
+function addEightyGrids() {
+  if (mainDiv.innerHTML == '') {
+    addElements(80)
+  } else {
+    mainDiv.innerHTML = '';
+    addElements(80);
+  }
+}
+
+function addNinetyGrids() {
+  if (mainDiv.innerHTML == '') {
+    addElements(90)
+  } else {
+    mainDiv.innerHTML = '';
+    addElements(90);
+  }
+}
+
+
 document.body.onload = addElements;
 const mainDiv = document.querySelector('.main-div');
 console.log(mainDiv)
@@ -18,5 +121,10 @@ function resetGrid() {
     mainDiv.innerHTML = '';
 }
 
-
-
+function startDrawing() {
+  const allGridDivs = document.querySelectorAll('.div-grids')
+  console.log(allGridDivs)
+  allGridDivs.forEach((element) => element.addEventListener('mouseenter', () => {
+    element.style.backgroundColor = 'black'
+  }))
+}
